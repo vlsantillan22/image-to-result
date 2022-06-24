331251f108da8ee36103b6b2d8e3c9c6215e59d7
Vincent <vincentlawrencesantillan@gmail.com>
2022-06-24 23:35:22 +0800

    add equation evaluator

--- app/src/main/java/com/vlsantillan/imagetoresult/ui/camera/CameraViewModel.kt
+++ app/src/main/java/com/vlsantillan/imagetoresult/ui/camera/CameraViewModel.kt
@@ -10,6 +10,9 @@ import com.vlsantillan.domain.usecase.CalculatorUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import net.objecthunter.exp4j.shuntingyard.ShuntingYard
+import net.objecthunter.exp4j.tokenizer.NumberToken
+import net.objecthunter.exp4j.tokenizer.OperatorToken
 import javax.inject.Inject
 
 /**
@@ -38,11 +41,36 @@ class CameraViewModel @Inject constructor(private val calculatorUseCase: Calcula
         textRecognizer.process(image)
             .addOnSuccessListener {
                 Log.e(CameraViewModel::class.java.simpleName, "Success: ${it.text}")
-                val x = it.text[0].toString().toFloat()
-                val y = it.text[2].toString().toFloat()
-                val op = it.text[1].toString()
-                val result = calculatorUseCase.calculate(x, y, op)
-                Log.e(CameraViewModel::class.java.simpleName, "Success Res: $result")
+                val tokens =
+                    ShuntingYard.convertToRPN(it.text.replace("x", "*"), emptyMap(), emptyMap(), emptySet(), true)
+
+                var input1: Double? = null
+                var input2: Double? = null
+                var operator = ""
+
+                if (tokens.size < 3) {
+                    return@addOnSuccessListener
+                }
+
+                tokens.asList().subList(0, 3).forEach { token ->
+                    when (token) {
+                        is OperatorToken -> {
+                            if (operator.isEmpty()) {
+                                operator = token.operator.symbol
+                            }
+                        }
+                        is NumberToken -> {
+                            if (input1 == null) {
+                                input1 = token.value
+                            } else {
+                                input2 = token.value
+                            }
+                        }
+                    }
+                }
+                if (input1 != null && input2 != null && operator.isNotEmpty()) {
+                    calculatorUseCase.calculate(input1!!.toFloat(), input2!!.toFloat(), operator)
+                }
             }
             .addOnFailureListener {
                 Log.e(CameraViewModel::class.java.simpleName, "Error: ${it.message}")
